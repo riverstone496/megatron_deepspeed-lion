725f6721a8524464f94323f6a96b4b2ce221e2d2
riverstone496 <robosatoki@outlook.jp>
2024-07-22 15:21:25 +0900

    resolve symboloc link in deepspeed

---
+++ deepspeed/ops/op_builder/npu/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+'''Copyright The Microsoft DeepSpeed Team'''
+
+from .fused_adam import FusedAdamBuilder
+from .no_impl import NotImplementedBuilder
+from .cpu_adam import CPUAdamBuilder
+from .cpu_adagrad import CPUAdagradBuilder
+from .cpu_lion import CPULionBuilder
